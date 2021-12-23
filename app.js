987e6d82fe172af17183d0bc1d3117111b5d1081
Volodymyr Kolchin <vkolchin@codifi.me>
2021-12-23 11:37:52 +0200

    initial commit

--- app.js
+++ app.js
@@ -1,73 +1,103 @@
 const http = require('http');
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const cors = require('cors');
 dotenv.config();
 
 const BigCommerce = require('node-bigcommerce');
 const bigCommerce = new BigCommerce({
-  clientId: process.env.CLIENTID,
-  accessToken: process.env.ACCESSTOKEN,
-  storeHash: process.env.STOREHASH,
-  responseType: 'json'
+    clientId: 'kfnfy9fisjsuqaa8ki6op8fx0sww49l',
+    accessToken: '5zjwikcnycybjiupfu9x90ccn4cjc6b',
+    storeHash: 'u3omg5mo4v',
+    responseType: 'json',
 });
-
-const accountSid = process.env.ACCOUNTSID;
-const authToken = process.env.AUTHTOKEN;
-const client = require('twilio')(accountSid, authToken);
-
+const bigCommerceV3 = new BigCommerce({
+    clientId: 'kfnfy9fisjsuqaa8ki6op8fx0sww49l',
+    accessToken: '5zjwikcnycybjiupfu9x90ccn4cjc6b',
+    storeHash: 'u3omg5mo4v',
+    responseType: 'json',
+    apiVersion: 'v3' // Default is v2
+});
 const app = express();
 
 app.use(bodyParser.json())
 
-  bigCommerce.get('/hooks')
-    .then(data => {
-      let webhooks = data;
-      let scopes = webhooks.map(a => a.scope);
-      const hookBody = {
-        "scope": "store/customer/created",
-        "destination": "https://7a821ff0.ngrok.io/webhooks",
-        "is_active": true
-      }
+    bigCommerce.get('/hooks')
+        .then(data => {
+            let webhooks = data;
+            let scopes = webhooks.map(a => a.scope);
+            const hookBody = {
+                "scope": "store/order/created",
+                "destination": "https://0036-46-211-224-248.ngrok.io/webhooks",
+                "is_active": true
+            }
 
-      console.log(scopes);
+        console.log(scopes);
 
-      if (scopes.indexOf("store/customer/created") > -1 || scopes.indexOf("store/customer/*") > -1) {
-        console.log("Customer webhook already exists");
-      } else {
-          bigCommerce.post('/hooks', hookBody)
-            .then(data => {
-                console.log('Customer webhook created');
-            })
-      }
+        if (scopes.indexOf("store/order/created") > -1 || scopes.indexOf("store/order/*") > -1) {
+            console.log("Order webhook already exists");
+        } else {
+            bigCommerce.post('/hooks', hookBody)
+                .then(data => {
+                    console.log('Order webhook created');
+                }).catch((error) => {
+                    console.log('Error: (webhook created)', error);
+                })
+        }
 
-    });
+        }).catch((error) => {
+            console.log('Error: (bigCommerce.get(/hooks)', error);
+        });
 
     app.post('/webhooks', function (req, res) {
         res.send('OK');
         let webhook = req.body;
-        let customerId = webhook.data.id;
-        //console.log(webhook);
-        console.log(customerId);
+        let orderId = webhook.data.id;
 
-        bigCommerce.get(`/customers/${customerId}`)
-        .then(data => {
-          let firstName = data.first_name;
-          let lastName = data.last_name;
-          sendText(firstName, lastName);
-          })
-        });
-    
-    function sendText(firstName, lastName){
-        client.messages
-        .create({
-            body: `${firstName} ${lastName} just registered an account on your store!`,
-            from: '+15127777777',
-            to: '+15125555555'
-        })
-        .then(message => console.log(message.sid));
-    }
+        //version 2
+        bigCommerce.get(`/orders/${orderId}/products`)
+            .then(data => {
+                let product = [];
+                data.forEach((el)=>{
+                    let product_options = [];
+                    el.product_options.forEach((index)=>{
+                        product_options.push(`{"display_name": "${index.display_name}", "display_value": "${index.display_value}"}`)
+                    })
+                   product.push(`{"id": "${el.id}", "name": "${el.name}", "base_price": "${el.base_price}", "quantity": "${el.quantity}", "product_options": [${product_options.toString()}]}`);
+                });
+                /**/
+                bodyText = {
+                    "permission_set": "write_and_sf_access",
+                    "namespace": "Product Data",
+                    "key": "id",
+                    "value": "["+product.toString()+"]",
+                    "description": "el.name",
+                    "resource_type": "order",
+                    "resource_id": orderId
+                }
 
-http.createServer(app).listen(1337, () => {
-  console.log('Express server listening on port 1337');
+                //version 3
+                bigCommerceV3.post(`/orders/${orderId}/metafields`, bodyText)
+                    .then(response => {
+                        console.log('response.data', response.data);
+                    }).catch((error) => {
+                        console.log('Error: BigCommerceV3.post', error);
+                    })
+            }).catch((error) => {
+                console.log('Error: BigCommerceV2.get', error);
+            });
+    });
+    app.get('/orderProduct', cors(), function (req, res) {
+        bigCommerceV3.get(`/orders/${req.query.order_id}/metafields`)
+            .then(response => {
+                console.log('response btn', response);
+                res.send(JSON.stringify(response));
+            }).catch((error) => {
+                console.log('Error: BigCommerceV3.get', error);
+            });
+    });
+
+http.createServer(app).listen(3000, () => {
+    console.log('Express server listening on port 3000');
 });
