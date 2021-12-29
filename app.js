65ce51ffb35059c7c7bb2e803f19e2db58e86d83
Volodymyr Kolchin <vkolchin@codifi.me>
2021-12-29 14:31:36 +0200

    fix bug

--- app.js
+++ app.js
@@ -3,23 +3,35 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const crypto = require('crypto');
+const useragent = require('express-useragent');
+const ip_address = null;
+// Defining key
+const secret = 'Hi';
+
+// Calling createHash method
+const hash = crypto.createHash('sha256', secret).digest('hex');
+
+// Displays output
+console.log(hash);
 dotenv.config();
 
 const BigCommerce = require('node-bigcommerce');
 const bigCommerce = new BigCommerce({
-    clientId: 'kfnfy9fisjsuqaa8ki6op8fx0sww49l',
-    accessToken: '5zjwikcnycybjiupfu9x90ccn4cjc6b',
-    storeHash: 'u3omg5mo4v',
+    clientId: 'im323r7hwohik173g1i9ztxjza3vx6l',
+    accessToken: 'sf4tp902y47cne0yxmu80la3h15g7ww',
+    storeHash: '85kzbf18qd',
     responseType: 'json',
 });
 const bigCommerceV3 = new BigCommerce({
-    clientId: 'kfnfy9fisjsuqaa8ki6op8fx0sww49l',
-    accessToken: '5zjwikcnycybjiupfu9x90ccn4cjc6b',
-    storeHash: 'u3omg5mo4v',
+    clientId: 'im323r7hwohik173g1i9ztxjza3vx6l',
+    accessToken: 'sf4tp902y47cne0yxmu80la3h15g7ww',
+    storeHash: '85kzbf18qd',
     responseType: 'json',
     apiVersion: 'v3' // Default is v2
 });
 const app = express();
+app.use(useragent.express());
 
 app.use(bodyParser.json())
 
@@ -29,7 +41,7 @@ app.use(bodyParser.json())
             let scopes = webhooks.map(a => a.scope);
             const hookBody = {
                 "scope": "store/order/created",
-                "destination": "https://0036-46-211-224-248.ngrok.io/webhooks",
+                "destination": "https://23dd-188-230-124-168.ngrok.io/webhooks",
                 "is_active": true
             }
 
@@ -54,44 +66,54 @@ app.use(bodyParser.json())
         res.send('OK');
         let webhook = req.body;
         let orderId = webhook.data.id;
+        let browser = req.useragent.browser;
+        let version = req.useragent.version;
+        //version 2 IP
+        bigCommerce.get(`/orders/${orderId}`)
+        .then(data => {
+            const ip_address = data.ip_address;
+            return ip_address;
+        }).then((IP) => {
+            //version 2
+            bigCommerce.get(`/orders/${orderId}/products`)
+                .then(data => {
+                    let product = [];
+                    data.forEach((el)=>{
+                        let product_options = [];
+                        el.product_options.forEach((index)=>{
+                            product_options.push(`{"display_name": "${index.display_name}", "display_value": "${index.display_value}"}`)
+                        })
+                       product.push(`{"id": "${el.id}", "name": "${el.name}", "base_price": "${el.base_price}", "quantity": "${el.quantity}", "product_options": [${product_options.toString()}], "hash": "${hash}","browser": "${browser}" , "version": "${version}", "ip_address" :"${IP}"}`);
+                    });
+                    /**/
+                    bodyText = {
+                        "permission_set": "write_and_sf_access",
+                        "namespace": "Product Data",
+                        "key": "id",
+                        "value": "["+product.toString()+"]",
+                        "description": "el.name",
+                        "resource_type": "order",
+                        "resource_id": orderId
+                    }
 
-        //version 2
-        bigCommerce.get(`/orders/${orderId}/products`)
-            .then(data => {
-                let product = [];
-                data.forEach((el)=>{
-                    let product_options = [];
-                    el.product_options.forEach((index)=>{
-                        product_options.push(`{"display_name": "${index.display_name}", "display_value": "${index.display_value}"}`)
-                    })
-                   product.push(`{"id": "${el.id}", "name": "${el.name}", "base_price": "${el.base_price}", "quantity": "${el.quantity}", "product_options": [${product_options.toString()}]}`);
+                    //version 3
+                    bigCommerceV3.post(`/orders/${orderId}/metafields`, bodyText)
+                        .then(response => {
+                        }).catch((error) => {
+                            console.log('Error: BigCommerceV3.post', error);
+                        })
+                }).catch((error) => {
+                    console.log('Error: BigCommerceV2.get', error);
                 });
-                /**/
-                bodyText = {
-                    "permission_set": "write_and_sf_access",
-                    "namespace": "Product Data",
-                    "key": "id",
-                    "value": "["+product.toString()+"]",
-                    "description": "el.name",
-                    "resource_type": "order",
-                    "resource_id": orderId
-                }
+        }).catch((error) => {
+            console.log('Error: BigCommerceV2.get', error);
+        });
 
-                //version 3
-                bigCommerceV3.post(`/orders/${orderId}/metafields`, bodyText)
-                    .then(response => {
-                        console.log('response.data', response.data);
-                    }).catch((error) => {
-                        console.log('Error: BigCommerceV3.post', error);
-                    })
-            }).catch((error) => {
-                console.log('Error: BigCommerceV2.get', error);
-            });
     });
     app.get('/orderProduct', cors(), function (req, res) {
+
         bigCommerceV3.get(`/orders/${req.query.order_id}/metafields`)
             .then(response => {
-                console.log('response btn', response);
                 res.send(JSON.stringify(response));
             }).catch((error) => {
                 console.log('Error: BigCommerceV3.get', error);
